4dd7ea55ebf31abe9ef8bf3c06889dd23b57aae0
Tehetena Mashresha <tehetena.mashresha@semel.fi>
2020-12-01 14:42:08 +0200

    Initial Commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testtut.Data;
+using testtut.Entities;
+
+namespace testtut.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController: ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task< ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+             return await _context.Users.ToListAsync();
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
