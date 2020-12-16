16558e49c05cad6609483b9d817fc5a329d13a08
Tehetena Mashresha <tehetena.mashresha@semel.fi>
2020-12-16 18:03:10 +0200

    https serving and bootstraping

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Client App';
+  user: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+  getUsers() {
+    this.http.get('https://localhost:44370/api/users').subscribe(res => {
+      console.log(res);
+      this.user = res;
+    }, error => { console.error(); }
+
+    )
+  }
+
 }
